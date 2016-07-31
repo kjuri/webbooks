94f736a101679231bc0ec3afc41be5406644d637
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-07-31 22:49:10 +0200

    avatar added to User
    avatar added to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable, :confirmable
 
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
+
   ROLES = %i(admin writer reader).freeze
 
   def roles=(roles)
