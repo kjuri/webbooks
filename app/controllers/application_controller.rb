08b09dee25cd5526220d31376216c7a2fb3e0027
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-08-01 00:46:17 +0200

    Users::RegistrationForm added to handle registration process
    Users::RegistrationForm added to handle registration process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:email, :password, :password_confirmation, :first_name,
-                         :last_name, :birthday, :username, roles: [])
+                         :last_name, :birthday, :username, :roles)
     end
   end
 end
---
+++ app/forms/users/registration_form.rb
@@ -0,0 +1,29 @@
+class Users::RegistrationForm < Reform::Form
+  property :first_name
+  property :last_name
+  property :email
+  property :password
+  property :password_confirmation
+  property :birthday
+  property :username
+  property :roles
+
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, confirmation: true,
+                       length: { minimum: 8 }
+  validates :roles, exclusion: { in: %w(admin) }
+
+  def active_for_authentication?
+    true
+  end
+
+  def authenticatable_salt
+  end
+
+  def save
+    return false unless valid?
+    sync
+    model.save
+  end
+end
