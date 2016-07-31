858e22b85466118aeb4fb6a5a1557429b4adc5da
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-07-31 22:12:31 +0200

    configure_permitted_parameters fix
    configure_permitted_parameters fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:email, :password, :password_confirmation, roles: [])
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation, roles: [])
     end
   end
 end
