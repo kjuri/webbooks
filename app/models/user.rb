4842853b954483f23c917848cd053a8f14554be8
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-07-31 21:49:44 +0200

    role based authorization init
    role based authorization init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable, :confirmable
+
+  ROLES = %i(admin writer reader).freeze
+
+  def roles=(roles)
+    roles = [*roles].map(&:to_sym)
+    self.roles_mask = (roles & ROLES).map do |r|
+      2**ROLES.index(r)
+    end.inject(0, :+)
+  end
+
+  def roles
+    ROLES.reject do |r|
+      ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+    end
+  end
+
+  def role?(role)
+    roles.include?(role)
+  end
 end
