8317a81a986ee9052dc726c9d4003e3a985a8e8b
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-08-07 21:41:32 +0200

    use SessionForm when logging in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,16 @@
 class Users::SessionsController < Devise::SessionsController
-# before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    build_resource(sign_in_params)
+    clean_up_passwords(resource)
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  protected
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def build_resource(hash = nil)
+    self.resource = Users::SessionForm.new(resource_class.new)
+  end
 end
