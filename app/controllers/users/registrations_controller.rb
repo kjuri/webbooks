86e10df4df1dfa77f3325b4cdfece3958234a30f
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-08-07 23:38:11 +0200

    permitted params for registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, only: [:create, :update]
+
   # POST /resource
   def create
     build_resource(sign_up_params)
@@ -19,6 +21,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation,
+               :birthday, :username, roles: [])
+    end
+  end
     root_path
   end
 end
