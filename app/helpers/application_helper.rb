f46f10b773f71d01928ed26c743434cbca23b23d
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-08-22 22:16:30 +0200

    dismissible flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_type(flash)
+    return 'success' if flash.notice
+    return 'warning' if flash.alert
+    return 'danger' if flash.error
+    'info'
+  end
 end
