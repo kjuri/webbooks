bd84c5a438c8cba4ff152cd7f394b95c85bd8135
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-08-23 23:10:39 +0200

    book form improvements

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,34 @@
 class BooksController < ApplicationController
   def new
-    @book = BookForm.new(Book.new)
+    @book = create_new_form
+  end
+
+  def create
+    @book = create_new_form
+    if @book.validate(params[:book])
+      @book.save do |data|
+        Book.create(data)
+        flash[:notice] = 'Book successfully created'
+        redirect_to book_path(@book)
+      end
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book
+    @book ||= Book.find(params[:id])
+  end
+
+  helper_method :book
+
+  def create_new_form
+    BookForm.new(Book.new)
+  end
+
+  def create_edit_form
+    BookForm.new(book)
   end
 end
--- app/forms/book_form.rb
+++ app/forms/book_form.rb
@@ -4,4 +4,11 @@ class BookForm < BaseForm
   property :description
 
   collection :chapters, form: ChapterForm
+  collection :genres do
+    property :name
+  end
+
+  validates :title, presence: true
+  validates :short_description, presence: true, length: { maximum: 255 }
+  validates :description, presence: true
 end
