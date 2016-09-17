9f997393d4aaec0ef18e2b2e8718c9b49088e3d8
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-09-17 23:05:13 +0200

    rescue AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = "You have no rights to access #{exception.action} " \
+                    "action for #{exception.subject.model.class.name}"
+    redirect_back(fallback_location: root_path)
+  end
+
   def current_user
     UserDecorator.decorate(super) unless super.nil?
   end
