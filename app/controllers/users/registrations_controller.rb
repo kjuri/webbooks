9f3a8729982f0d30dc6363ef3d0ec93e5fb805a0
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-09-20 23:27:56 +0200

    maybe that

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,8 +10,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if Rails.env.development?
       render text: confirmation_url(resource.model,
                                     confirmation_token: resource.model.confirmation_token)
-    else
-      resource.model.send_confirmation_instructions
     end
   end
 
