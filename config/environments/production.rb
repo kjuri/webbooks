f26cf37d50028338a4aa6b806ef64794dc532a73
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-09-20 22:55:34 +0200

    default_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
       :authentication       => :plain,
       :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { :host => 'http://leszczyna.wzks.uj.edu.pl/11_szmigiel/webbooks' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
