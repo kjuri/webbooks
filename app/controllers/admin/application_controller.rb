46ffbd07b3d69a2e47a3eafc6d6d5719078fac53
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-09-21 23:11:47 +0200

    admin authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,7 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      raise CanCan::AccessDenied unless current_user && current_user.admin?
     end
 
     # Override this value to specify the number of elements to display at a time
