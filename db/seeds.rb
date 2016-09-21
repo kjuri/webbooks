8a514064c5521d8c7a25b2a37bb73a0f565e4ecd
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-09-21 23:24:54 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
-admin = User.new(email: 'admin@example.com', password: 'adminadmin',
-                 password_confirmation: 'adminadmin', first_name: 'Admin',
-                 last_name: 'Admin', roles: 'admin')
 
+if Rails.env.development?
+  admin = User.new(email: 'admin@example.com', password: 'adminadmin',
+                   password_confirmation: 'adminadmin', first_name: 'Admin',
+                   last_name: 'Admin', roles: 'admin')
+else
+  admin = User.new(email: 'admin@webbooks.com', password: 'adminwebbooks',
+                   password_confirmation: 'adminwebbooks', first_name: 'Admin',
+                   last_name: 'Admin', roles: 'admin')
+end
 admin.skip_confirmation!
 admin.save
