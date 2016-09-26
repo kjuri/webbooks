bc300c08fdc832e6ce5a044749bffe653f090201
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2016-09-26 19:17:36 +0200

    root_path overwritten for production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     return 'danger' if flash.error
     'info'
   end
+
+  def root_path
+    if Rails.env.production?
+      Rails.application.routes.url_helpers.root_url(host: 'leszczyna.wzks.uj.edu.pl/11_szmigiel/webbooks')
+    else
+      Rails.application.routes.url_helpers.root_path
+    end
+  end
 end
